3b100acf53072dce3db8a15edcfeca8de1fcdd4f
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-02-03 21:30:09 +0530

    data

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button signup_btn,login_btn,dashboard_btn,about_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findingViews();
+
+
+
+        
+    }
+
+    private void findingViews() {
+        signup_btn=findViewById(R.id.signup_btn);
+        login_btn=findViewById(R.id.login_btn);
+        dashboard_btn=findViewById(R.id.dashboard_btn);
+        about_btn=findViewById(R.id.about_btn);
     }
 }
