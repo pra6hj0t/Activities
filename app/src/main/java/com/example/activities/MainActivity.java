2cf09a2f6260b0cfa269d617573808352da7de8f
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-02-05 13:49:49 +0530

    data

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         signup_btn.setOnClickListener(view->{
             Intent intent = new Intent(this, Signup.class);
             startActivity(intent);
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        dashboard_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
+        about_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, About.class);
+                startActivity(intent);
+            }
         });
 
         
