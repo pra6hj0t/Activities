f70a07150c985ce9312db094e892f20dd06af9e6
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-02-07 07:36:47 +0530

    data

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.activities.data.User;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         findingViews();
 
         signup_btn.setOnClickListener(view->{
@@ -62,5 +64,35 @@ public class MainActivity extends AppCompatActivity {
         login_btn=findViewById(R.id.login_btn);
         dashboard_btn=findViewById(R.id.dashboard_btn);
         about_btn=findViewById(R.id.about_btn);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 }
